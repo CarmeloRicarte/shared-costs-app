6a06fed05f660ad1693bbe9a209c43a650a08cca
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2023-02-19 18:36:09 +0100

    feat: add data for costs

---
+++ src/pages/Home/data/data.ts
@@ -0,0 +1,32 @@
+import { Cost } from '../models/Cost';
+
+export const Costs: Cost[] = [
+  {
+    id: '1',
+    personName: 'Jerrilee Trenam',
+    totalAmount: 54.58,
+    description: 'Cena',
+    paymentDate: '10/12/2022',
+  },
+  {
+    id: '2',
+    personName: 'Paddy Trathan',
+    totalAmount: 37.01,
+    description: 'Cine',
+    paymentDate: '01/11/2023',
+  },
+  {
+    id: '3',
+    personName: 'Nefen Daburn',
+    totalAmount: 57.73,
+    description: 'Taxi',
+    paymentDate: '02/08/2023',
+  },
+  {
+    id: '4',
+    personName: 'Yalonda Cowdray',
+    totalAmount: 15.24,
+    description: 'Compra supermercado',
+    paymentDate: '03/12/2023',
+  },
+];
