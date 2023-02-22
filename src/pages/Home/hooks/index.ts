6781eed7911418757b952a6223b81debb4292591
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2023-02-22 19:23:38 +0100

    feat: create hook for manage costs

---
+++ src/pages/Home/hooks/index.ts
@@ -0,0 +1,4 @@
+export * from './useBalances';
+export * from './useCosts';
+export * from './useFriends';
+export * from './useGroups';
---
+++ src/pages/Home/hooks/useCosts.ts
@@ -0,0 +1,34 @@
+import { useLocalStorage } from '../../../hooks';
+import { useHomeContext } from '../context';
+import { Cost } from '../models';
+import { getCosts } from '../services';
+import { addCost } from '../services/Costs.service';
+
+export const useCosts = () => {
+  const { costs, setCosts } = useHomeContext();
+  const { setItem, getItem } = useLocalStorage();
+
+  const setCostsContextAndLocalStorage = (costs: Cost[]) => {
+    setCosts(costs);
+    setItem('costs', costs);
+  };
+
+  const getAllCosts = async () => {
+    const costs = await getCosts();
+    costs ? setCostsContextAndLocalStorage(costs) : setCosts(getItem('costs'));
+  };
+
+  const createCost = async (cost: Cost) => {
+    const createdCost = await addCost(cost);
+    if (createdCost) {
+      const newCosts = [...costs, cost];
+      setCostsContextAndLocalStorage(newCosts);
+    }
+  };
+
+  return {
+    costs,
+    getAllCosts,
+    createCost,
+  };
+};
